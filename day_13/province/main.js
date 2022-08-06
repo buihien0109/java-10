afda260c0b9126905eef10175891b897f5f62bbe
buihien0109 <buihien01091997@gmail.com>
2022-08-06 09:32:18 +0700

    update

--- day_13/province/main.js
+++ day_13/province/main.js
@@ -6,3 +6,64 @@
 
 // Lấy danh sách xã phường
 // GET : https://provinces.open-api.vn/api/d/${districtCode}?depth=2
+
+const provinceEl = document.querySelector("#province");
+const districtEl = document.querySelector("#district");
+
+// Lấy danh sách tỉnh thành phố
+const getProvince = async () => {
+    try {
+        let res = await axios.get("https://provinces.open-api.vn/api/p/");
+        console.log(res);
+
+        renderProvince(res.data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Lấy danh sách tỉnh thành phố
+const getDistrict = async (provinceCode) => {
+    try {
+        let res = await axios.get(`https://provinces.open-api.vn/api/p/${provinceCode}?depth=2`);
+        console.log(res);
+
+        renderDistrict(res.data.districts)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Hiển thị quận huyện
+const renderDistrict = arr => {
+    districtEl.innerHTML = "";
+
+    let html = "<option hidden>-- Chọn quận huyện</option>";
+    arr.forEach(p => {
+        html += `<option value=${p.code}>${p.name}</option>`
+    });
+    districtEl.innerHTML = html;
+}
+
+// Hiển thị tỉnh thành phố
+const renderProvince = arr => {
+    provinceEl.innerHTML = "";
+
+    let html = "<option hidden>-- Chọn tỉnh thành phố</option>";
+    arr.forEach(p => {
+        html += `<option value=${p.code}>${p.name}</option>`
+    });
+    provinceEl.innerHTML = html;
+}
+
+// Lắng nghe sự kiện khi thay đổi tỉnh thành phố
+provinceEl.addEventListener("change", () => {
+    // Lấy ra code của tỉnh thành phố đang chọn
+    let provinceCode = provinceEl.value;
+    console.log(provinceCode)
+
+    // Lấy ra danh sách quận huyện tương ứng -> hiển thị
+    getDistrict(provinceCode);
+})
+
+getProvince();
