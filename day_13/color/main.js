81603b745240a11fc201728eb22e788534f7bd3d
buihien0109 <buihien01091997@gmail.com>
2022-08-08 15:08:51 +0700

    update

--- day_13/color/main.js
+++ day_13/color/main.js
@@ -2,6 +2,15 @@ const btnRandomColorName = document.querySelector("#btn-random-color-name");
 
 btnRandomColorName.addEventListener("click", async () => {
     try {
+        // Demo POST request
+        // let res = await axios.post("http://localhost:8080/bmi-post", {
+        //     height: 1.6,
+        //     weight: 50
+        // });
+        // console.log(res)
+
+        // renderBmi(res.data);
+
         let res = await axios.get("http://localhost:8080/random-color?type=4");
         console.log(res);
 
@@ -12,6 +21,16 @@ btnRandomColorName.addEventListener("click", async () => {
         alert(error.response.data.message);
     }
 })
+
+const renderBmi = (data) => {
+    // Hiển thị BMI
+
+    if (data <= 18.5) {
+        // Hiển thị message
+    } else if (data > 18.5 && data <= 29.9) {
+        // Hiển thị message
+    }
+}
 
 // console.log(btnRandomColorName.getAttribute("typeColor"));
 console.log(btnRandomColorName.dataset.type);
