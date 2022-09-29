a690fa27c26c5b537c81e623c92d51b50492c271
buihien0109 <buihien01091997@gmail.com>
2022-09-29 21:21:57 +0700

    update

--- day_27/springsecurity-user/src/main/java/com/example/springsecuritybasic/security/SecurityConfig.java
+++ day_27/springsecurity-user/src/main/java/com/example/springsecuritybasic/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springsecuritybasic.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,6 +12,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -18,12 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("bob").password(passwordEncoder().encode("111")).roles("USER")
-                .and()
-                .withUser("alice").password(passwordEncoder().encode("111")).roles("USER", "EDITOR")
-                .and()
-                .withUser("anna").password(passwordEncoder().encode("111")).roles("USER", "EDITOR", "ADMIN");
+        auth
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
