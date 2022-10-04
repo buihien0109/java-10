2c96e5b6742d1b3220d35a2ec4fd60e50cc53f40
buihien0109 <buihien01091997@gmail.com>
2022-10-04 19:54:38 +0700

    update

--- day_27/register-user/src/main/java/com/example/registeruser/service/AuthService.java
+++ day_27/register-user/src/main/java/com/example/registeruser/service/AuthService.java
@@ -1,19 +1,48 @@
 package com.example.registeruser.service;
 
+import com.example.registeruser.exception.NotFoundException;
 import com.example.registeruser.request.LoginRequest;
 import com.example.registeruser.request.RegisterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
 @Service
 public class AuthService {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     public String login(LoginRequest request, HttpSession session) {
-        return null;
+        try {
+            // 1. Tạo đối tượng xác thực
+            UsernamePasswordAuthenticationToken token =
+                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+
+            // 2. Tiến hành xác thực
+            Authentication authentication = authenticationManager.authenticate(token);
+
+            // 3. Lưu đối tượng vào trong context
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            // 4. Set cookie
+            session.setAttribute("MY_SESSION", authentication.getName());
+
+            return "Đăng nhập thành công";
+        }
+        catch (AuthenticationException e) {
+            throw new NotFoundException("Tài khoản hoặc mật khẩu không chính xác");
+        }
     }
 
     public String logout(HttpSession session) {
-        return null;
+        session.invalidate();
+        return "Đăng xuất thành công";
     }
 
     public String register(RegisterRequest request) {
--- day_27/register-user/src/test/java/com/example/registeruser/RegisterUserApplicationTests.java
+++ day_27/register-user/src/test/java/com/example/registeruser/RegisterUserApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.registeruser;
 
+import com.example.registeruser.entity.User;
+import com.example.registeruser.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
 
 @SpringBootTest
 class RegisterUserApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void save_users() {
+        User user1 = User.builder()
+                .name("Bui Hien")
+                .email("hien@gmail.com")
+                .password(passwordEncoder.encode("111"))
+                .roles(List.of("USER", "EDITOR", "ADMIN"))
+                .enabled(true)
+                .build();
+
+        User user2 = User.builder()
+                .name("Pham Man")
+                .email("man@gmail.com")
+                .password(passwordEncoder.encode("111"))
+                .roles(List.of("USER", "EDITOR"))
+                .enabled(true)
+                .build();
+
+        User user3 = User.builder()
+                .name("Duc Thinh")
+                .email("thinh@gmail.com")
+                .password(passwordEncoder.encode("111"))
+                .roles(List.of("USER"))
+                .enabled(true)
+                .build();
+
+        userRepository.saveAll(List.of(user1, user2, user3));
+    }
 }
